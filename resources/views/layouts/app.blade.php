a0e2e0185b9ed032145d738f971eb611b0e0829f
jimmy-martin <jimmy.martin952@gmail.com>
2021-10-20 10:45:46 +0200

    styling app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Video Games</title>
+</head>
+<body class="bg-gray-900 text-white">
+    <header class="border-b border-gray-800">
+        <nav class="container mx-auto flex items-center justify-between px-4 py-6">
+            <div class="flex items-center">
+                <a href="/">
+                    <img src="/images/laracasts-logo.svg" alt="laracasts" class="w-32 flex-none">
+                </a>
+                <ul class="flex ml-16 space-x-8">
+                    <li><a href="#" class="hover:text-gray-400">Games</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Coming soon</a></li>
+                </ul>
+            </div>
+            <div class="flex items-center">
+                <div class="relative">
+                    <input type="text" class="bg-gray-800 text-sm rounded-full w-64 px-3 py-1 focus:outline-none focus:ring pl-8" placeholder="Search...">
+                    <div class="absolute top-0 flex items-center h-full ml-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="fill-current text-gray-400 w-4" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                </div>
+                <div class="ml-6">
+                    <a href="#"><img src="/images/avatar.jpg" alt="avatar" class="rounded-full w-8"></a>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main class="py-8">
+        @yield('content')
+    </main>
+
+    <footer class="border-t border-gray-800">
+        <div class="container mx-auto px-4 py-6">
+            Powered By <a href="#" class="underline hover:text-gray-400">IGDB API</a>
+        </div>
+    </footer>
+</body>
+</html>
