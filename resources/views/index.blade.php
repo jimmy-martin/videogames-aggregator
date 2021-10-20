f48c679dad21c0c7fb0f9906d2a03414af65f2d8
jimmy-martin <jimmy.martin952@gmail.com>
2021-10-20 12:10:00 +0200

    update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container mx-auto px-4">
         <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular Games</h2>
-        <div class="popular-games text-sm grid grid-cols-6 gap-12 border-b border-gray-800 pb-16">
+        <div class="popular-games text-sm grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16 justify-items-center">
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
@@ -174,8 +174,8 @@
             </div>
         </div>
 {{--        end popular-games--}}
-        <div class="flex my-10">
-            <div class="recently-reviewed w-3/4 mr-32">
+        <div class="flex flex-col lg:flex-row my-10">
+            <div class="recently-reviewed w-full lg:w-3/4 mr-0 lg:mr-32">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
                 <div class="recently-reviewed-container space-y-12 mt-8">
                     <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
@@ -192,7 +192,7 @@
                                 Final Fantasy VII Remake
                             </a>
                             <div class="text-gray-400 mt-1">Playstation 4</div>
-                            <p class="mt-6 text-gray-400">
+                            <p class="mt-6 text-gray-400 hidden lg:block">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda deserunt ipsam iure perferendis quae recusandae similique voluptatem. Aliquid eligendi hic nulla obcaecati rem! Animi blanditiis eos illo in inventore iure libero maiores, molestiae mollitia natus neque nesciunt nostrum perspiciatis, porro praesentium quia quo saepe sequi soluta tenetur ut veritatis voluptatibus voluptatum! Accusantium mollitia nemo nihil nisi praesentium quas tenetur totam.
                             </p>
                         </div>
@@ -211,7 +211,7 @@
                                 Animal Crossing: New Horizons
                             </a>
                             <div class="text-gray-400 mt-1">Nintendo Switch</div>
-                            <p class="mt-6 text-gray-400">
+                            <p class="mt-6 text-gray-400 hidden lg:block">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda deserunt ipsam iure perferendis quae recusandae similique voluptatem. Aliquid eligendi hic nulla obcaecati rem! Animi blanditiis eos illo in inventore iure libero maiores, molestiae mollitia natus neque nesciunt nostrum perspiciatis, porro praesentium quia quo saepe sequi soluta tenetur ut veritatis voluptatibus voluptatum! Accusantium mollitia nemo nihil nisi praesentium quas tenetur totam.
                             </p>
                         </div>
@@ -230,7 +230,7 @@
                                 Doom Eternal
                             </a>
                             <div class="text-gray-400 mt-1">Playstation 4, PC</div>
-                            <p class="mt-6 text-gray-400">
+                            <p class="mt-6 text-gray-400 hidden lg:block">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda deserunt ipsam iure perferendis quae recusandae similique voluptatem. Aliquid eligendi hic nulla obcaecati rem! Animi blanditiis eos illo in inventore iure libero maiores, molestiae mollitia natus neque nesciunt nostrum perspiciatis, porro praesentium quia quo saepe sequi soluta tenetur ut veritatis voluptatibus voluptatum! Accusantium mollitia nemo nihil nisi praesentium quas tenetur totam.
                             </p>
                         </div>
@@ -238,7 +238,7 @@
                 </div>
             </div>
             {{--        end recently-reviewed--}}
-            <div class="most-anticipated w-1/4">
+            <div class="most-anticipated lg:w-1/4 mt-12 lg:mt-0">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Most Anticipated</h2>
                 <div class="most-anticipated-container space-y-10 mt-8">
                     <div class="game flex">
