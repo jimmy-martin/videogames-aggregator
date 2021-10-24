21de121d1c70223ea448ea767e8e939de8b01e1e
jimmy-martin <jimmy.martin952@gmail.com>
2021-10-24 13:20:26 +0200

    skeleton refactor

---
+++ resources/views/components/game-card-small-skeleton.blade.php
@@ -0,0 +1,9 @@
+<div class="game flex">
+    <div class="bg-gray-800 w-16 h-20 flex-none text-transparent">
+        Image
+    </div>
+    <div class="ml-4">
+        <div class="text-transparent bg-gray-700 rounded leading-tight">Title goes here today</div>
+        <div class="inline-block text-transparent bg-gray-700 rounded mt-2">Date goes here</div>
+    </div>
+</div>
--- resources/views/livewire/most-anticipated.blade.php
+++ resources/views/livewire/most-anticipated.blade.php
@@ -4,15 +4,7 @@
         <x-game-card-small :game="$game" />
     @empty
         @foreach (range(1,4) as $game)
-        <div class="game flex">
-            <div class="bg-gray-800 w-16 h-20 flex-none text-transparent">
-                Image
-            </div>
-            <div class="ml-4">
-                <div class="text-transparent bg-gray-700 rounded leading-tight">Title goes here today</div>
-                <div class="inline-block text-transparent bg-gray-700 rounded mt-2">Date goes here</div>
-            </div>
-        </div>
+        <x-game-card-small-skeleton />
         @endforeach
     @endforelse
 
